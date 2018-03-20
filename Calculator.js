4518d22226ce8276757b307c09a069696c013c41
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-20 16:33:18 -0400

    Added . value to the end of the input selectors.  Changed the formulas.  Added a console log to beginning and ending to see if there was anything there. There isn't.

--- Calculator.js
+++ Calculator.js
@@ -1,19 +1,14 @@
 // The selectors
-const beginning = document.getElementById("beginning");
-const ending = document.getElementById("ending");
+const beginning = document.getElementById("beginning").value;
+const ending = document.getElementById("ending").value;
 const button = document.querySelector("button");
 const answer = document.getElementById("answer");
 
-// The values to be used
-const  test = Number(beginning.style.textContent);
-const test2 = Number(ending.style.textContent);
-
-
-// formulas
-const formula = test2 - test;
-const ans = formula/test;
-
+// The formulas
+const subtract = ending - beginning;
+const ans = subtract/beginning;
 // For now it's an alert, to better see if it is working. 
 button.addEventListener("click", function(){
-    alert(ans, test, test2);
+    alert(ans);
+    console.log(ending, beginning);
 });
