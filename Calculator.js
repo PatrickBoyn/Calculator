966a3554de17d44ec3b10174520db4d80a378cd4
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-19 22:48:26 -0400

    Added a test to see if the js was connected. It wasn't.

--- Calculator.js
+++ Calculator.js
@@ -1,3 +1,5 @@
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 const answer = document.getElementById("answer");
+
+answer.textContent = "This is a test. ";
