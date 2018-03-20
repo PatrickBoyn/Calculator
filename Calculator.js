815ba8dfd7d027a288ac6edc45e56e909b2045c7
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-19 23:02:27 -0400

    For the fun of it I connected everything with something.

--- Calculator.js
+++ Calculator.js
@@ -5,3 +5,8 @@ const answer = document.getElementById("answer");
 
 beginning.value = "This is a test. ";
 ending.value = "This is also a test. ";
+answer.textContent = "Yet another test. ";
+
+button.addEventListener("click", function(){
+    alert("I am connected!");
+});
