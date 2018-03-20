911dc60737bc788ce173120880c2622bfb6010a0
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-20 15:33:25 -0400

    Added comments to the variables. Also in the last file I deleted a typo.

--- Calculator.js
+++ Calculator.js
@@ -1,10 +1,14 @@
+// The selectors
 const beginning = document.getElementById("beginning");
 const ending = document.getElementById("ending");
 const button = document.querySelector("button");
 const answer = document.getElementById("answer");
 
+// The values to be used
 const  test = beginning.value = 5;
 const test2 = ending.value = 10;
+
+// formulas
 const formula = test2 - test;
 const ans = formula/test;
 
