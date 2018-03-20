4836ad849ac402299b9a18d0581eaf621c82097f
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-20 15:32:12 -0400

    The logic does work, but the refactor also looks nicer.

--- Calculator.js
+++ Calculator.js
@@ -5,7 +5,7 @@ const answer = document.getElementById("answer");
 
 const  test = beginning.value = 5;
 const test2 = ending.value = 10;
-const formula = test2 - test1;
+const formula = test2 - test;
 const ans = formula/test;
 
 button.addEventListener("click", function(){
