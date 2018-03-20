57305b86dfadc2cfa89d7f735beefc288cde37df
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-19 23:04:15 -0400

    Testing how to manipulate the answer id.

--- Calculator.js
+++ Calculator.js
@@ -3,9 +3,9 @@ const ending = document.getElementById("ending");
 const button = document.querySelector("button");
 const answer = document.getElementById("answer");
 
-beginning.value = "This is a test. ";
+const  test = beginning.value = "This is a test. ";
 ending.value = "This is also a test. ";
-answer.textContent = "Yet another test. ";
+answer.textContent = test;
 
 button.addEventListener("click", function(){
     alert("I am connected!");
