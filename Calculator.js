eea4388d4c0b8be33468b049bda952f5ef240a53
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-20 14:07:05 -0400

    Trying to manipulate answer viia a button.

--- Calculator.js
+++ Calculator.js
@@ -3,10 +3,10 @@ const ending = document.getElementById("ending");
 const button = document.querySelector("button");
 const answer = document.getElementById("answer");
 
-const  test = beginning.value = "This is a test. ";
-ending.value = "This is also a test. ";
-answer.textContent = test;
+const  test = beginning.value = 5;
+const test2 = ending.value = 10;
+
 
 button.addEventListener("click", function(){
-    alert("I am connected!");
+   answer =  test - test2/test;
 });
