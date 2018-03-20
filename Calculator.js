e81879c9861fe7f4379bb4fc6974b50421c49f9d
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-19 22:50:17 -0400

    Still toying with how to connect things.

--- Calculator.js
+++ Calculator.js
@@ -2,4 +2,4 @@ const input = document.querySelector("input");
 const button = document.querySelector("button");
 const answer = document.getElementById("answer");
 
-answer.textContent = "This is a test. ";
+answer.textContent = "Hello World!";
