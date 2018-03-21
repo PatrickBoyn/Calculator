fba5419fca01dad8193c35173d51c058531ad56b
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-21 17:37:44 -0400

    Trying to use logic I've used before to make this work.

--- Calculator.js
+++ Calculator.js
@@ -3,12 +3,16 @@ const beginning = document.getElementById("beginning").value;
 const ending = document.getElementById("ending").value;
 const button = document.querySelector("button");
 const answer = document.getElementById("answer");
+const input = document.querySelector("beginning");
 
 // The formulas
 const subtract = ending - beginning;
 const ans = subtract/beginning;
 // For now it's an alert, to better see if it is working. 
 button.addEventListener("click", function(){
-    alert(ans);
-    console.log(ending, beginning);
+    input.addEventListener("change", function(){
+        answer.textContent = input.value;
+    });
+    
 });
+
