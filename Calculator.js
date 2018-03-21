96a933a916e3b83285afa2f61bcd2358ed6ca4d6
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-21 17:57:15 -0400

    Still working out the old logic.

--- Calculator.js
+++ Calculator.js
@@ -11,8 +11,8 @@ const ans = subtract/beginning;
 // For now it's an alert, to better see if it is working. 
 button.addEventListener("click", function(){
     input.addEventListener("change", function(){
-        answer.textContent = input.value;
+        answer.textContent = Number(input.value);
     });
-    
+
 });
 
