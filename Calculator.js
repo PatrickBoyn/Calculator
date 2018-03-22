7402c0078cde0e566cc243718f3d9f51058ad81b
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-21 20:10:09 -0400

    This is just to test to see if I have both inputs setup correctly.

--- Calculator.js
+++ Calculator.js
@@ -3,6 +3,7 @@ const beginning = document.getElementById("beginning");
 const ending = document.getElementById("ending");
 const button = document.querySelector("button");
 const answer = document.getElementById("answer");
+const answer2 = document.getElementById("answer2");
 
 // The formulas
 const subtract = ending - beginning;
@@ -10,5 +11,6 @@ const ans = subtract/beginning;
 // For now it's an alert, to better see if it is working. 
 button.addEventListener("click", function(){
    answer.textContent = Number(beginning.value);
+   answer2.textContent = Number(ending.value);
 });
 
