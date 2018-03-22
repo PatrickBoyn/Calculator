f860ea7748a67df9a965bf52338abf6451cf11dd
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-21 20:11:40 -0400

    Deleted the tests.

--- Calculator.js
+++ Calculator.js
@@ -3,7 +3,6 @@ const beginning = document.getElementById("beginning");
 const ending = document.getElementById("ending");
 const button = document.querySelector("button");
 const answer = document.getElementById("answer");
-const answer2 = document.getElementById("answer2");
 
 // The formulas
 const subtract = ending - beginning;
@@ -11,6 +10,5 @@ const ans = subtract/beginning;
 // For now it's an alert, to better see if it is working. 
 button.addEventListener("click", function(){
    answer.textContent = Number(beginning.value);
-   answer2.textContent = Number(ending.value);
 });
 
