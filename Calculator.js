5b538852f159c6127b97b2f21a143d6bd49669c9
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-21 20:07:23 -0400

    Deleted an extra input variable linked to beginning. Also deleted the variable. It is no longer needed.

--- Calculator.js
+++ Calculator.js
@@ -3,14 +3,12 @@ const beginning = document.getElementById("beginning");
 const ending = document.getElementById("ending");
 const button = document.querySelector("button");
 const answer = document.getElementById("answer");
-var input = document.querySelector("beginning");
 
 // The formulas
 const subtract = ending - beginning;
 const ans = subtract/beginning;
 // For now it's an alert, to better see if it is working. 
 button.addEventListener("click", function(){
-    var test = answer.textContent = Number(beginning.value);
-    console.log(test)
+   answer.textContent = Number(beginning.value);
 });
 
